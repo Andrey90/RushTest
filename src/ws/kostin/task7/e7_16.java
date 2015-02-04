01b3d80a63d93b01ff90a056630f3855642598bd
Admin <luchkovsky@mail.com>
2015-02-04 17:56:47 +0200

    Циклы в Java/ 16 задача Массивы в Java/ примеры задач

--- src/ws/kostin/task7/e7_16.java
+++ src/ws/kostin/task7/e7_16.java
@@ -14,9 +14,44 @@ package ws.kostin.task7;
  */
 public class e7_16 {
     public static void main(String[] args) {
-        int rez = 0;
+//        int rez = 0;
+//        int i;
+//        int rez1 = 0;
+//        int count = 0;
 //        for (i = 1; i < 100000; i++){
+//            int i1 = i / 100000 % 10;
+//            int i2 = i / 10000 % 10;
+//            int i3 = i / 1000 % 10;
+//            int i4 = i / 100 % 10;
+//            int i5 = i / 10 % 10;
+//            int i6 = i % 10;
 //
+//            if (i1 != 4 & i2 != 4 & i3 != 4 & i4 != 4 & i5 != 4 & i6 != 4){
+//                int k1 = i % 100;
+//                int k2 = i % 1000 / 10;
+//                int k3 = i % 10000 / 100;
+//                int k4 = i / 1000;
+//
+//                if (k1 != 13 & k2 != 13 & k3 != 13 & k4 != 13){
+//                    rez++;
+//                }
+//            }
 //        }
+//        rez1 = i - rez;
+//        System.out.println(rez1);
+        int count = 0;
+        for (int i = 1; i < 100000; i++){
+            String numberStr = Integer.toString(i);
+            if(numberStr.contains("4") || numberStr.contains("13")){
+                count++;
+                System.out.println(numberStr);
+            }
+
+        }
+
+        System.out.println("*****************************************");
+        System.out.println("count = " + count);
     }
 }
+
+
---
+++ src/ws/kostin/task8/e8_1.java
@@ -0,0 +1,27 @@
+package ws.kostin.task8;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 04.02.15
+ * Time: 12:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class e8_1 {
+    public static void main(String args[]){
+        int a[];
+        double ar1[];
+        double [] ar2 = {3.14, 2.43, -2.4, 33.4323};
+
+        a = new int[10];
+        int n = 5;
+        ar1 = new double[n];
+
+        System.out.println(+ ar2[1]);
+
+        ar2[0] = 5;
+        ar2[1] = 1;
+
+        System.out.println(ar2[0]);
+    }
+}
