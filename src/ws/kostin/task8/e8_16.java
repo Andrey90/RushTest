ec4f7f471e66c7b5616dc911cca6ea7c8d17b417
Admin <luchkovsky@mail.com>
2015-03-12 11:31:37 +0200

    Массивы/ Задача 16/ закончить

--- src/ws/kostin/task8/e8_16.java
+++ src/ws/kostin/task8/e8_16.java
@@ -10,12 +10,21 @@ public class e8_16 {
     public static void main(String args[]){
         int mas[][] = new int[7][4];
         int i, j;
+        int pr = 0;
+        int mas1[] = new int[mas.length];
 
         for (i = 0; i < mas.length; i++){
+            System.out.print("Nomer stroki: " + i + " ");
             for (j = 0; j < mas[i].length; j++){
                 //Min + (int)(Math.random() * ((Max - Min) + 1))
                 mas[i][j] = (int)(Math.random() * 11) - 5;
                 System.out.print(mas[i][j] + " ");
+                //Заполним второй массив, путем перемножения всех элементов определенной строки двумерного массива
+                if (j == 0){
+                    mas1[i] = mas[i][j];
+                }else {
+
+                }
             }
             System.out.println();
         }
