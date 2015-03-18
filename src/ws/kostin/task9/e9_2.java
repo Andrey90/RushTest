45c640f08e68b00573b5f04ddb953f80e0ebadc9
luchkovsky <aluchkovsky90@gmail.com>
2015-03-18 23:17:55 +0200

    Рекурсия/задача 3

--- src/ws/kostin/task9/e9_2.java
+++ src/ws/kostin/task9/e9_2.java
@@ -1,5 +1,7 @@
 package ws.kostin.task9;
 
+import java.util.Scanner;
+
 /**
  * Created by luchkovsky on 17.03.15.
  */
@@ -11,6 +13,31 @@ public class e9_2 {
         return fibonachi(a - 1) + fibonachi(a - 2);
     }
     public static void main(String args[]){
-        System.out.print(fibonachi(46));
+        int rez = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Vvedite znachenie: ");
+        int d = scanner.nextInt();
+
+        if (d < 45 && d > 0){
+            rez = fibonachi(d);
+            System.out.print(rez);
+        }
+        else {
+            if (d > 0){
+                int x1 = 1;
+                int x2 = 1;
+                rez = 0;
+                for (int i = 3; i <= d; i++){
+                    rez = x1 + x2;
+                    x1 = x2;
+                    x2 = rez;
+                }
+                System.out.print(rez);
+            }
+            else {
+                System.out.print("Vvedite korectnoe znachenie!");
+            }
+        }
+
     }
 }
